c0ff8a5c9a4baa0714204ec6204410903f53c325
Nelson Soto <nelsonsoto22@gmail.com>
2022-05-31 19:03:33 -0400

    Setting up SignalR Hub

---
+++ ChatroomWebApp/Hubs/ChatHub.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatroomWebApp.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task SendMessage(string user, string message)
+    {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+    public async Task SendMessageGroup(string group, string user, string message)
+    {
+        await Clients.Groups(group).SendAsync("ReceiveMessage", user, message);
+    }
+}
